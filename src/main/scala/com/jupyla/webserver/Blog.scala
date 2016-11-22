16e30fc485d9fafc294e6a432507c66ec550d6f8
ice48623 <ice48623@hotmail.com>
2016-11-22 21:12:07 +0700

    First Commit

---
+++ src/main/scala/com/jupyla/webserver/Blog.scala
@@ -0,0 +1,38 @@
+package com.jupyla.webserver
+
+import org.scalatra._
+import org.zeromq.ZMQ
+import org.zeromq.ZMQ.{Context,Socket}
+
+class Blog extends JupylaStack {
+  object HelloWorldClient{
+  def main(args : Array[String]) {
+    //  Prepare our context and socket
+    val context = ZMQ.context(1)
+    val socket = context.socket(ZMQ.REQ)
+
+    println("Connecting to hello world server…")
+    socket.connect ("tcp://localhost:8888")
+
+    //  Do 10 requests, waiting each time for a response
+        for (request_nbr <- 1 to 10)  {
+      //  Create a "Hello" message.
+      //  Ensure that the last byte of our "Hello" message is 0 because
+      //  our "Hello World" server is expecting a 0-terminated string:
+            val request = "Hello ".getBytes()
+      request(request.length-1)=0 //Sets the last byte to 0
+      // Send the message
+      println("Sending request " + request_nbr + "…") + request.toString
+      socket.send(request, 0)
+
+      //  Get the reply.
+      val reply = socket.recv(0)
+      //  When displaying reply as a String, omit the last byte because
+      //  our "Hello World" server has sent us a 0-terminated string:
+      println("Received reply " + request_nbr + ": [" + new String(reply,0,reply.length-1) + "]")
+    }
+  }
+}
+
+
+}
